02f103e31b18fce0f46e037de6d0ec52dc42c04c
acopar <andrej.copar@gmail.com>
2015-06-08 12:51:48 +0200

    Windows, set path for current session.

--- pygraphviz.bat
+++ pygraphviz.bat
@@ -1,3 +1,4 @@
 C:\Python34\Scripts\pip.exe install pygraphviz-1.3rc2-cp34-none-win32.whl
 reg add HKCU\Environment /v PATH /d "C:\Program Files (x86)\Graphviz2.38\bin;C:\Program Files\Graphviz2.38\bin;%path%" /f
+PATH "C:\Program Files (x86)\Graphviz2.38\bin;C:\Program Files\Graphviz2.38\bin;%path%"
 pause
